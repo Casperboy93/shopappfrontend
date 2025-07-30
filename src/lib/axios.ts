49fa5d6dfcb14c2441a0d9875bb49595edad341b
mac-med <mohammed.jaize1993@gmail.com>
2025-07-30 22:11:43 +0100

    axios fix to adapt to railways db

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,7 @@
-// src/lib/axios.ts
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Add token to headers

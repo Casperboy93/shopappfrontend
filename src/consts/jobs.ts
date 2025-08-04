6589d4d7d0337d244eab19c39317f9972c132503
Casperboy93 <mohammed.jaize1993@gmail.com>
2025-08-04 22:04:55 +0100

    fix translation

--- src/consts/jobs.ts
+++ src/consts/jobs.ts
@@ -1,15 +1,22 @@
+import { useTranslation } from 'react-i18next';
+
 export const JOB_TYPES = [
-    'Electrician', 'Plumber', 'Carpenter', 'Painter', 'Cleaner',
-    'Gardener', 'Mechanic', 'AC Technician', 'Mover', 'Welder',
-    'Hairdresser', 'Makeup Artist', 'Tailor', 'Baker', 'Cook',
-    'Babysitter', 'Security Guard', 'Mason', 'Driver', 'Delivery',
-    'IT Technician', 'Photographer', 'Videographer', 'DJ',
-    'Event Planner', 'Interior Designer', 'Housekeeper', 'Dog Walker',
-    'Tutor', 'Personal Trainer', 'Laundry Service', 'Nanny',
-    'Construction Worker', 'Tile Installer', 'Roofer', 'Window Cleaner',
-    'Car Washer', 'Mobile Repair', 'Electric Gate Installer',
-    'Glass Installer', 'Swimming Pool Cleaner', 'Ironworker',
-    'Refrigerator Repair', 'Shoe Repair', 'Locksmith', 'Car Upholsterer',
-    'Seamstress', 'Massage Therapist', 'Manicurist', 'Barber'
-  ];
+  'jobs.electrician', 'jobs.plumber', 'jobs.carpenter', 'jobs.painter', 'jobs.cleaner',
+  'jobs.gardener', 'jobs.mechanic', 'jobs.acTechnician', 'jobs.mover', 'jobs.welder',
+  'jobs.hairdresser', 'jobs.makeupArtist', 'jobs.tailor', 'jobs.baker', 'jobs.cook',
+  'jobs.babysitter', 'jobs.securityGuard', 'jobs.mason', 'jobs.driver', 'jobs.delivery',
+  'jobs.itTechnician', 'jobs.photographer', 'jobs.videographer', 'jobs.dj',
+  'jobs.eventPlanner', 'jobs.interiorDesigner', 'jobs.housekeeper', 'jobs.dogWalker',
+  'jobs.tutor', 'jobs.personalTrainer', 'jobs.laundryService', 'jobs.nanny',
+  'jobs.constructionWorker', 'jobs.tileInstaller', 'jobs.roofer', 'jobs.windowCleaner',
+  'jobs.carWasher', 'jobs.mobileRepair', 'jobs.electricGateInstaller',
+  'jobs.glassInstaller', 'jobs.swimmingPoolCleaner', 'jobs.ironworker',
+  'jobs.refrigeratorRepair', 'jobs.shoeRepair', 'jobs.locksmith', 'jobs.carUpholsterer',
+  'jobs.seamstress', 'jobs.massageTherapist', 'jobs.manicurist', 'jobs.barber'
+];
+
+export const useTranslatedJobTypes = () => {
+  const { t } = useTranslation();
+  return JOB_TYPES.map(jobKey => t(jobKey));
+};
   
